e621e1583c1d49ec2959016f5c3ebf510e75b7e3
Fürész Tibor Teodor <jaoycb@inf.elte.hu>
2024-05-24 20:16:14 +0200

    Main File
    Makes the package line, imports parent and interface packages, creates
    class.

---
+++ main.cpp
@@ -0,0 +1,278 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <utility>
+#include <tuple>
+
+using namespace std;
+
+// using capital letters to avoid conflict
+enum ProtectionLevel
+{
+    Public,
+    Protected,
+    Private,
+    Default
+};
+
+void testNull(size_t pos)
+{
+    if (pos == string::npos)
+    {
+        cout << "Not found" << endl;
+        exit(1);
+    }
+}
+
+vector<string> split(string s, char delimiter)
+{
+    vector<string> result;
+    size_t pos = 0;
+    while ((pos = s.find(delimiter)) != string::npos)
+    {
+        result.push_back(s.substr(0, pos));
+        s.erase(0, pos + 1);
+    }
+    result.push_back(s);
+    return result;
+}
+
+ProtectionLevel protlvl(string line) // 1 == true, 0 == false
+{
+    // search for VISIBLE_TO_ALL VISIBLE_TO_SUBCLASSES VISIBLE_TO_NONE
+    if (line.find("VISIBLE_TO_ALL") != string::npos)
+    {
+        return Public;
+    }
+    else if (line.find("VISIBLE_TO_SUBCLASSES") != string::npos)
+    {
+        return Protected;
+    }
+    else if (line.find("VISIBLE_TO_NONE") != string::npos)
+    {
+        return Private;
+    }
+    else
+    {
+        return Default;
+    }
+}
+
+int createVar(fstream file)
+{
+    return 0;
+}
+
+int createMethod(fstream file)
+{
+    return 0;
+}
+
+int createConstructor(fstream file)
+{
+    return 0;
+}
+
+string concat(vector<string> v, char delimiter)
+{
+    string result;
+    for (int i = 0; i < v.size(); i++)
+    {
+        result += v[i];
+        if (i != v.size() - 1)
+        {
+            result += delimiter;
+        }
+    }
+    return result;
+}
+
+tuple<ofstream *, string, string> createFile(string line)
+{
+    size_t pos = line.find('"');
+    testNull(pos);
+    size_t pos2 = line.find('"', pos + 1);
+    testNull(pos2);
+    string className = line.substr(pos + 1, pos2 - pos - 1);
+    pos = className.find_last_of('.');
+    string name = className.substr(pos + 1);
+    vector<string> package = split(className.substr(0, pos), '.');
+    /*
+    for(int i = 0; i < package.size(); i++){
+        cout << "package " << package[i] << endl;
+    }
+    cout << "name " << name << endl;
+    cout << "Found theClass " << className << endl;
+    */
+    // create file
+    string place;
+    for (int i = 0; i < package.size(); i++)
+    {
+        place += package[i];
+        place += "/";
+    }
+    string command = "mkdir -p " + place;
+    system(command.c_str());
+    place += name;
+    place += ".java";
+    ofstream *file = new ofstream(place);
+    if (!file)
+    {
+        cout << "womp womp" << endl;
+        exit(1);
+    }
+    *file << "package ";
+    for (int i = 0; i < package.size(); i++)
+    {
+        *file << package[i];
+        if (i != package.size() - 1)
+        {
+            *file << ".";
+        }
+    }
+    *file << ";\n";
+    return make_tuple(file, name, concat(package, '.'));
+}
+
+int main()
+{
+    string line;
+    string stopString = "public static void init";
+    // fstream file("VariableStructureTest.java");
+    // fstream file("HeuristicStructureTest.java");
+    fstream file("EqualityConstraintStructureTest.java");
+    // fstream file("BruteForceHeuristicStructureTest.java");
+
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+            if (line.find(stopString) != string::npos)
+            {
+                break;
+            }
+        }
+        getline(file, line);
+        string properties;
+        size_t pos = line.find('"');
+        testNull(pos);
+        auto [sfile, name, package] = createFile(line);
+        cout << "Created file " << name << endl;
+        *sfile << endl;
+        bool needImport = false;
+        vector<string> imports;
+        if (line.find("theClass") != string::npos)
+        {
+            string parent = "";
+            string interface = "";
+            // search for withInterface or withParent in line
+            string s = "withParent(\"";
+            pos = line.find(s);
+            if (pos != string::npos)
+            {
+                parent = " extends ";
+                string tempparent = line.substr(pos + s.length());
+                pos = tempparent.find('"');
+                tempparent = tempparent.substr(0, pos);
+                pos = tempparent.find_last_of('.');
+                parent += tempparent.substr(pos + 1);
+                tempparent = tempparent.substr(0, pos);
+                if (tempparent != package)
+                {
+                    imports.push_back(tempparent);
+                    needImport = true;
+                }
+                cout << "Found theParent " << parent << " " << tempparent << endl;
+            }
+            s = "withInterface(\"";
+            pos = line.find(s);
+            if (pos != string::npos)
+            {
+                interface = " implements ";
+                string tempinterface = line.substr(pos + s.length());
+                pos = tempinterface.find('"');
+                tempinterface = tempinterface.substr(0, pos);
+                pos = tempinterface.find_last_of('.');
+                interface += tempinterface.substr(pos + 1);
+                tempinterface = tempinterface.substr(0, pos);
+                if (tempinterface != package)
+                {
+                    imports.push_back(tempinterface);
+                    needImport = true;
+                }
+                cout << "Found theInterface " << interface << endl;
+            }
+            getline(file, line);
+            string vis;
+            switch (protlvl(line))
+            {
+            case Public:
+                vis = "public ";
+                break;
+            case Protected:
+                vis = "protected ";
+                break;
+            case Private:
+                vis = "private ";
+                break;
+            case Default:
+                cout << "default" << endl;
+                break;
+            }
+            if (needImport)
+            {
+                for (int i = 0; i < imports.size(); i++)
+                {
+                    *sfile << "import ";
+                    *sfile << imports[i];
+                    *sfile << ";\n";
+                }
+                *sfile << ";\n";
+            }
+            *sfile << vis << "class " << name << parent << interface << " {\n";
+        }
+        else if (line.find("theInterface") != string::npos)
+        {
+            bool hasParent = false;
+            string parent;
+            string s = "withParent(\"";
+            pos = line.find(s);
+            if (pos != string::npos)
+            {
+                parent = line.substr(pos + s.length());
+                pos = parent.find('"');
+                parent = parent.substr(0, pos);
+                cout << "Found theParent " << parent << endl;
+            }
+            line = line.substr(pos);
+        }
+        // search for it.hasMethod it.hasConstructor it.hasField
+        // just seach for the string will call the function later
+        while (getline(file, line))
+        {
+            if (line.find("it.hasMethod") != string::npos)
+            {
+                cout << "Found method" << endl;
+            }
+            else if (line.find("it.hasConstructor") != string::npos)
+            {
+                cout << "Found constructor" << endl;
+            }
+            else if (line.find("it.hasField") != string::npos)
+            {
+                cout << "Found field" << endl;
+            }
+        }
+        file.close();
+        sfile->close(); // Close the ofstream properly
+        delete sfile;   // Delete the ofstream object to prevent memory leaks
+    }
+    else
+    {
+        std::cout << "Unable to open file";
+    }
+
+    return 0;
+}
